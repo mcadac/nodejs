156926e1e81159a56c17a3e975e27484eb99b6bf
Camilo Espitia <david.espitia@Camilos-MacBook-Pro.local>
2019-08-26 14:24:02 -0400

    Add express demo example

--- express-demo/README.md
+++ express-demo/README.md
@@ -2,6 +2,50 @@
 
 https://www.npmjs.com/package/express
 
+## Express configuration
+- Enable Json in express
+```
+//This line is necessary to enable Json 
+app.use(express.json());
+```
+
+## Node package used with express
+- For input validations
+```
+npm install joi
+```
+
+## Middleware
+- A Middlewar is a function executed in the pipeline process between a request and a response 
+- For example the express.json() function
+- We can create a Middleware with the next fuction definition
+```
+express.use(expressObj.json());
+express.use(expressObj.urlencoded({ extended: true }));
+express.use(expressObj.static('foler-name'))
+express.use(
+    function(req,res,next){
+        console.log('Any process here');
+        //This line is so important to continue with the pipeline process and others middleware
+        next();
+    }
+
+);
+```
+- Thrid-party middleware
+
+```
+node i helmet //Help with security headers
+node i morgan //Help to log request message information
+
+const helmet = require('helemt');
+const morgan = require('morgan');
+
+app.use(helmet());
+app.use(morgan('tiny'));
+
+```
+
 ## How to read params and response with an object
 
 ```
@@ -17,6 +61,7 @@ app.get('/api/courses/:id', (req, res) => {
     res.send(course);
 });
 ```
+
 
 
 
--- express-demo/index.js
+++ express-demo/index.js
@@ -1,8 +1,21 @@
 /**
 * @author milo
 */  
+const logger = require('./logger.js');
+const Joi = require('joi');
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
 const app = express();
+
+
+//This line is necessary to enable Json 
+app.use(express.json());
+app.use(logger);
+app.use(express.static('public'));
+app.use(helmet());
+app.use(morgan('tiny'));
+
 
 const object = [
     {id: 1, name: 'nodejs'},
@@ -16,7 +29,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1,2,3]);
+    
+    res.send(object);
 });
  
 app.get('/api/courses/:id', (req, res) => {
@@ -35,6 +49,72 @@ app.get('/api/posts/:name/:month', (req, res) => {
     //res.send(req.params);
     res.send(req.query);
 });
+
+app.post('/api/courses', (req, res) => {
+
+    const { error } =  isCourseValid(req.body);
+
+    if(error){
+        console.log(error);
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    const newCourse = {
+        id: object.length + 1,
+        name: req.body.name
+    };
+
+    object.push(newCourse);
+    res.send(newCourse);
+});
+
+app.put('/api/courses/:id', (req, res) => {
+    
+    const id = req.params.id;
+    const course = object.find(c => c.id === parseInt(id));
+    
+    if(!course){
+        return resp.status(404).send('Course not found');
+    };
+
+    const { error } = isCourseValid(req.body);
+
+    if(error){
+        console.log(error);
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    course.name = req.body.name;
+    return res.send(course);    
+
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+
+    const id =  parseInt(req.params.id);
+    const course = object.find(o => o.id === id);
+    
+    if(!course){
+        return res.status(404).send('Course not found');
+    }
+
+    const index = object.indexOf(course);
+    console.log(index);
+    object.splice(index, 1);
+
+    res.send(object);
+});
+
+function isCourseValid(course){
+
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);
+};
 
 // Read an envirotment variable
 const port = process.env.PORT || 3000;
